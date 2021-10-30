a7a32dcfe489c7bc9e2ce48b7c0c04d860c261d2
FilipKochan <filda.koch@gmail.com>
2021-10-31 00:42:48 +0200

    update

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -4,15 +4,14 @@ import { Typography } from '@mui/material'
 import { Link } from 'react-router-dom'
 import './header.scss'
 
-
 const Header = () => {
     return (
         <Box>
             <Box className="navbar">
-                <Link to='/'>Domů</Link>
-                <Link to='/add'>Přidat položku</Link>
-                <Link to='/list'>Historie transakcí</Link>
-                <Link to='/account'>Výpis účtu</Link>
+                <Link to='/brno-byt-cash/'>Domů</Link>
+                <Link to='/brno-byt-cash/add'>Přidat položku</Link>
+                <Link to='/brno-byt-cash/list'>Historie transakcí</Link>
+                <Link to='/brno-byt-cash/account'>Výpis účtu</Link>
             </Box>
             <Typography component='h1' sx={{ textAlign: 'center', fontSize: '2.5rem', fontWeight: 'bold', marginBottom: '2rem', marginTop: '5rem' }}>Where's my money?</Typography>
         </Box>
